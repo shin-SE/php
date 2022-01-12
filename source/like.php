d3f2ff2bdcc225deb05efbc51d0b370193d66cff
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-12 11:12:55 +0900

    Update like.php

--- source/like.php
+++ source/like.php
@@ -1,57 +1,66 @@
 <?php
-$ip = $_SERVER['REMOTE_ADDR'];
+
 if(isset($_POST['like'])){
-    $likeid = $_POST['like'];
-    
-    $title= __FILE__;
-	$table_id=substr($title,-11,7);
-
-    $result = mysqli_query($conn,"select like_cnt from threadno".$newname." where colume_id= '$likeid' ");
-    $row = mysqli_fetch_assoc($result);
-    echo $row['like_cnt'];
-  
-     if(!$row['like_cnt']){
-            echo '<br>ƒGƒ‰[FŒ»İthreadnoƒe[ƒuƒ‹ƒAƒNƒZƒXo—ˆ‚Ü‚¹‚ñ<br>';
-     }else{
+     if(isset($_SESSION['id'])){
+         $id= $_SESSION['id'];
+         $likeid = $_POST['like'];
          
-            $ip_sql=mysqli_query($conn,"select ip from like_ip where thread_id='$table_id' AND colume_id='$likeid' and ip='$ip'");
-            $count=mysqli_num_rows($ip_sql);
-            
-            
-          if($count==0){              // like+1
-            
-                            //update trd table
-                  $like_count=$row['like_cnt']+1;
-                  $stmt = $mysqli->prepare( "update threadno".$table_id." set like_cnt=? where colume_id=?'");
-                  $stmt->bind_param('ii',$like_count,$likeid);
-                  $stmt->execute();
-                  
-                  
-                             //insert into threadno table
-                  $stmt= $mysqli->prepare("insert into like_ip (thread_id,colume_id,ip) values (?,?,?)");
-                  $stmt->bind_param('iis','$table_id','$likeid','$ip');
-                  $stmt->execute();
+         $title= __FILE__;
+         $table_id=substr($title,-11,7);
 
-                  unset ($_POST['like']);
-            
-           }else if($count==1){                        //like -1
+         $result = mysqli_query($conn,"select like_cnt from threadno".$newname." where colume_id= '$likeid' ");
+         $row = mysqli_fetch_assoc($result);
+         echo $row['like_cnt'];
+       
+         if(!$row['like_cnt']){
+                 
+                  '<br>ã‚¨ãƒ©ãƒ¼ï¼šç¾åœ¨threadnoãƒ†ãƒ¼ãƒ–ãƒ«ã‚¢ã‚¯ã‚»ã‚¹å‡ºæ¥ã¾ã›ã‚“<br>';
+         }else{
+              
+                 $ip_sql=mysqli_query($conn,"select user_id from like_ip where thread_id='$newname' AND colume_id='$likeid' and user_id='$id'");
+                 $count=mysqli_num_rows($ip_sql);
+                 
+                 echo $count;
+                 echo $newname;
+                 if($count==0){              // like+1
+                 
+                                 //update trd table
+                       $like_count=$row['like_cnt']+1;
+                       $stmt = $conn->prepare("UPDATE threadno".$newname." SET like_cnt=? where colume_id=?");
+                       $stmt->bind_param('ii',$like_count,$likeid);
+                       $stmt->execute();
+                       
+                       
+                                  //insert into like_ip table
+                       $stmt= $conn->prepare("insert into like_ip (thread_id,colume_id,user_id) values (?,?,?)");
+                       $stmt->bind_param('iis',$newname,$likeid,$id);
+                       $stmt->execute();
 
-                             //update trd table
-                  $like_count=$row['like_cnt']-1;
-                  
-                  $stmt = $conn->prepare( "update threadno".$newname." set like_cnt=? where colume_id=?'");
-                  $stmt->bind_param('ii',$like_count,$likeid);
-                  $stmt->execute();
-                  
-                  
-                              //delete threadno table
-                  $stmt= $mysqli->prepare("delete from like_ip where (thread_id=? AND colume_id=? )AND ip=?");
-                  $stmt->bind_param('iis','$table_id','$likeid','$ip');
-                  $stmt->execute();
+                       unset ($_POST['like']);
+                 
+                }else if($count==1){                        //like -1
 
-                  unset ($_POST['like']);
-            
+                                  //update trd table
+                       $like_count=$row['like_cnt']-1;
+                       
+                       $stmt = $conn->prepare("UPDATE threadno".$newname." SET like_cnt=? where colume_id=?");
+                       $stmt->bind_param('ii',$like_count,$likeid);
+                       $stmt->execute();
+                       
+                       
+                                   //delete threadno table
+                       $stmt= $conn->prepare("delete from like_ip where (thread_id=? AND colume_id=? )AND user_id=?");
+                       $stmt->bind_param('iis',$newname,$likeid,$id);
+                       $stmt->execute();
+
+                       unset ($_POST['like']);
+                 
+               }
           }
+     }else{
+          $alert="<script type='text/javascript'>alert('You must login first.');</script>";
+          echo $alert;
+          
      }
-     }
+   }
 ?>
