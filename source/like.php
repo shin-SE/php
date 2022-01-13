8a2173bcc1f9dd5711f1af934adbf45c673d6c9a
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-13 16:28:44 +0900

    Update like.php

--- source/like.php
+++ source/like.php
@@ -4,22 +4,25 @@ if(isset($_POST['like'])){
      if(isset($_SESSION['id'])){
          $id= $_SESSION['id'];
          $likeid = $_POST['like'];
-         
+         echo $_POST['like'];
          $title= __FILE__;
          $table_id=substr($title,-11,7);
 
          $result = mysqli_query($conn,"select like_cnt from threadno".$newname." where colume_id= '$likeid' ");
          $row = mysqli_fetch_assoc($result);
-       
-         if(!$row['like_cnt']){
+         echo $row['like_cnt'];
+         if(!$result){
                  
                   '<br>エラー：現在threadnoテーブルアクセス出来ません<br>';
          }else{
-              
-                 $ip_sql=mysqli_query($conn,"select user_id from like_ip where thread_id='$newname' AND colume_id='$likeid' and user_id='$id'");
-                 $count=mysqli_num_rows($ip_sql);
-                 
-
+                 echo $row['like_cnt'];
+                 $ip_sql=$conn->query("select user_id from like_ip where thread_id='$newname' AND colume_id='$likeid' AND user_id='$id'");
+                 if(!$ip_sql){
+                     $count=0;
+                     }else{
+                     $count=$ip_sql->num_rows;
+                  }
+                 echo "<br>count:".$count;
                  if($count==0){              // like+1
                  
                                  //update trd table
@@ -31,12 +34,12 @@ if(isset($_POST['like'])){
                        
                                   //insert into like_ip table
                        $stmt= $conn->prepare("insert into like_ip (thread_id,colume_id,user_id) values (?,?,?)");
-                       $stmt->bind_param('iis',$newname,$likeid,$id);
+                       $stmt->bind_param('iii',$newname,$likeid,$id);
                        $stmt->execute();
 
                        unset ($_POST['like']);
                  
-                }else if($count==1){                        //like -1
+                }else if($count!=0){                        //like -1
 
                                   //update trd table
                        $like_count=$row['like_cnt']-1;
