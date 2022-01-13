2b476f3688e6a70cc61698988e4564ad43f380fd
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-13 15:33:30 +0900

    Update mute.php

--- mute.php
+++ mute.php
@@ -1,36 +1,40 @@
 <?php
 
 if(isset($_POST['mute'])){
+
      if(isset($_SESSION['id'])){
          $id= $_SESSION['id'];
          $muteid = $_POST['mute'];
                 
-                 $stmt=mysqli_query($conn,"select user_id from mute where thread_id='$newname' AND colume_id='$muteid' and user_id='$id'");
-                 $count=mysqli_num_rows($stmt);
-                 
-
-                 if($count==0){              // mute
+                 $result_muting=$conn->query("select user_id from mute where thread_id='$newname' AND colume_id='$muteid' and user_id='$id'");
+                 $row_cnt_mute = $result_muting->num_rows;
+             
+                 if($row_cnt_mute==0){         // mute
                       
                        $stmt= $conn->prepare("insert into mute (thread_id,colume_id,user_id) values (?,?,?)");
-                       $stmt->bind_param('iis',$newname,$muteid,$id);
+                       $stmt->bind_param('iii',$newname,$muteid,$id);
                        $stmt->execute();
 
-                       unset ($_POST['mute']);
+                       
+                       
                  
-                }else if($count==1){                        //quit mute
+                }else{                        //quit mute
 
                        $stmt= $conn->prepare("delete from mute where (thread_id=? AND colume_id=? )AND user_id=?");
                        $stmt->bind_param('iis',$newname,$muteid,$id);
                        $stmt->execute();
 
-                       unset ($_POST['mute']);
                  
                }
-          }
-     }else{
-          $alert="<script type='text/javascript'>alert('You must login first.');</script>";
-          echo $alert;
+          }else if(!isset($_SESSION['id'])){
+              $alert="<script type='text/javascript'>alert('You must login first.mute.');</script>";
+               echo $alert;
           
      }
-   }
+
+unset($_POST['mute']);
+
+     }
+
+   
 ?>
