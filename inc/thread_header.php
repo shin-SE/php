7a640ab4580fa128d4599a22544b4303c2fa43f3
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-13 20:32:23 +0900

    Add files via upload

--- inc/thread_header.php
+++ inc/thread_header.php
@@ -10,11 +10,11 @@
 					if (isset($_SESSION['name'])){ 
 						//登録の場合
 						
-				?><a href="../source/thread_logout.php">ログアウト</a> &nbsp; &nbsp; &nbsp;| &nbsp;&nbsp; &nbsp;<a href="profile.php">プロファイル</a>
+				?><a href="../source/logout.php">ログアウト</a> &nbsp; &nbsp; &nbsp;| &nbsp;&nbsp; &nbsp;<a href="profile.php">プロファイル</a>
 				<?php
 					}else if($is_snyc==false){
 						//DBに接続してない場合
-				?><a href="../login.php">ログイン </a>|<a href="sign_up.php">会員登録</a>
+				?><a href="../login.php">ログイン </a>|<a href="../sign_up.php">会員登録</a>
 				<?php
 					};  //DBに接続した場合は何も表示しません
 				?>
