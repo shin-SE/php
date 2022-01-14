aa7562e08b4a955879d62e531b7e5c1a3165d566
Shikizakura <84487024+Shikizakura@users.noreply.github.com>
2022-01-14 16:44:18 +0900

    Add files via upload

--- source/commenting.php
+++ source/commenting.php
@@ -5,7 +5,8 @@
 		if (isset($_POST["body"])) {
 			$body = htmlspecialchars($_POST["body"], ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, "UTF-8", true);
 			
-		
+			// データベースに接続
+			include('dbconnect.php');
 			//日本東京タイムゾーン指定
 			date_default_timezone_set("Asia/Tokyo"); 
 
@@ -19,16 +20,17 @@
 			}
 			$time=date('Y-m-d H:i:s');
 			mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-			if (!$conn) {
+			$mysqli = mysqli_connect($host, $user, $password, $name);
+			if (!$mysqli) {
 				echo "データベース接続失敗: " . mysqli_connect_error();
 				die();
 			}
 			
 			try {
-				$sql="INSERT INTO ".$tablename." (user_name, content, posttime, anonymous) VALUES (?, ?, ?, ?);";
-				$stmt = mysqli_prepare($conn, $sql);
-				mysqli_stmt_bind_param($stmt, "ssss", $user_name, $body, $time, $anonymous);
+				$user_id = $_SESSION['id'];
+				$sql="INSERT INTO " . $tablename . " (user_id, user_name, content, posttime, anonymous) VALUES (?, ?, ?, ?, ?);";
+				$stmt = mysqli_prepare($mysqli, $sql);
+				mysqli_stmt_bind_param($stmt, "issss", $user_id, $user_name, $body, $time, $anonymous);
 				$res_comment_update = mysqli_stmt_execute($stmt);
 			} catch (mysqli_sql_exception $e) {
 				echo $e->getMessage();
@@ -40,18 +42,23 @@
 				//変数型変更 文字列newname --->INT newname_int
 				$newname_int=  (int)$newname;
 				//Trdテーブルの最新更新日を更新
-				$sql_Trd_update="UPDATE Trd SET last_post_time = ? WHERE thread_id = ?;";
-				$stmt2 = mysqli_prepare($conn, $sql_Trd_update);
+				$sql_Trd_update = "UPDATE Trd SET last_post_time = ? WHERE thread_id = ?;";
+				$stmt2 = mysqli_prepare($mysqli, $sql_Trd_update);
 				mysqli_stmt_bind_param($stmt2, "si", $time, $newname_int);
 				// 実行
 				$res_Trd_update = mysqli_stmt_execute($stmt2);
-				mysqli_close($conn);
+				mysqli_close($mysqli);
 				if ($res_Trd_update) {
-					header('Location: '.$newname.'.php');
+					var_dump($newname);
+					if (isset($_POST["body"])) {
+						unset($_POST["body"]);
+					} else {
+						echo '<script type="text/javascript">location.reload();</script>';
+					}
 					exit();
 				} else {
 					echo 'Trdテーブル更新失敗しました。';
-					// printf("Error message:%s\n",$conn->error);
+					// printf("Error message:%s\n",$musqli->error);
 				}
 			} else {
 				echo 'threadnoテーブル更新失敗しました。';
