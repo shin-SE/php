172c88e9a180806b0364870be1d8a3d42422c2d9
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-17 10:24:18 +0900

    Add files via upload

--- userlist.php
+++ userlist.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <?php
-ini_set("auto_detect_line_endings",true);
+    ini_set('display_errors', "On");
+    ini_set("auto_detect_line_endings",true);
 	$title = 'Bullentin board | Sin・System Engineers';
 	$description = 'フォロー管理';
 	$is_home = false; //トップページの判定用の変数
@@ -10,6 +11,7 @@ ini_set("auto_detect_line_endings",true);
 
 	// データベースに接続
 	include('source/dbconnect.php');
+	$conn=mysqli_connect($host,$user,$password,$name);
 ?>	
 	<!-- 特定のページでのみ読み込むスタイルシートなどがあればここに追加 -->
 </head>
@@ -22,17 +24,46 @@ ini_set("auto_detect_line_endings",true);
 			<li class="crumb">User List</li>
 		</ol>
 	</nav>
+
 	<section class="user01">
 		<div class="follow">
 			<h2>Follow list</h2>
+                <?php   
+                	$id=$_SESSION['id'];
+                	$stmt_follow = mysqli_prepare($conn, "SELECT user_name FROM user_kihon where user_id IN(SELECT follow_id FROM follow WHERE follower_id=?)");
+                	$stmt_follow->bind_param("i", $id);
+                    $stmt_follow->execute();
+                    $stmt_follow->bind_result($name);
+                    while ($stmt_follow->fetch()) {
+                        echo $name."<br>"; 
+                    }
+                ?>
 		</div>
 
-		<div class="follower">
+	<div class="follower">
 			<h2>Follower list</h2>
+                <?php   
+                	$stmt_follower = mysqli_prepare($conn, "SELECT user_name FROM user_kihon where user_id IN(SELECT follower_id FROM follow WHERE follow_id=?)");
+                	$stmt_follower->bind_param("i", $id);
+                    $stmt_follower->execute();
+                    $stmt_follower->bind_result($name);
+                    while ($stmt_follower->fetch()) {
+                        echo $name."<br>"; 
+                    }
+                	?>
 		</div>
 
 		<div class="block">
 			<h2>Block list</h2>
+                 <?php   
+                 	$stmt_block = mysqli_prepare($conn, "SELECT user_name FROM user_kihon where user_id IN(SELECT blockoppoment_id FROM block WHERE user_id=?)");
+                 	$stmt_block->bind_param("i", $id);
+                    $stmt_block->execute();
+                    $stmt_block->bind_result($name);
+                    while ($stmt_block->fetch()) {
+                          echo $name."<br>"; 
+                     }
+                 	?>
 		</div>
 	</section>
 	<?php include 'inc/footer.php'; ?> <!-- footer.php の読み込み -->
