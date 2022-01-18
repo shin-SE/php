c210423d5c50f025343e4dfbb79e1e6fdf4889ae
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-18 09:34:06 +0900

    Update dbconnect.php

--- source/dbconnect.php
+++ source/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-	
+//for test
     $host = 'localhost';
     $name= 'shineva';
     $charset='utf8';
@@ -8,5 +8,19 @@
     $dsn = 'mysql:'.$host.';dbname='.$name.';charset='.$charset ;
     $user = 'member';
     $password = 'member';
+    
+?>
+
+<?php
+	//for server
+//
+//    $host = 'mysql154.phy.lolipop.lan';
+//    $name= 'LAA1387111-shineva';
+//    $charset='utf8';
+
+    
+//    $dsn = 'mysql:'.$host.';dbname='.$name.';charset='.$charset ;
+//    $user = 'LAA1387111';
+//    $password = 'member2021';
     
 ?>
