53678a21e95d19cae1beb996787caff86891062b
H667K <84487022+H667K@users.noreply.github.com>
2022-01-19 12:14:54 +0900

    Add files via upload

--- thread_entertainment.php
+++ thread_entertainment.php
@@ -38,12 +38,16 @@
 		</ol>
 	</nav>
 	
-	<article>  
+	<article>
+	  
+	<div class="zenny_aside">
+	  <?php include './inc/aside.php'?>
+	</div> 
 		<!--スレッド表示   showthreads.php  の読み込み--->
 		<?php include 'source/showthreads.php'?>
 		<a href="index.php#postjump" class="btn btn--green btn--radius">スレッド投稿</a>
 	</article>
-	<?php include './inc/aside.php'?>
+	
 
     <!--ページングボタン     pagechange.php の読み込み  -->
 	<?php include 'source/pagechange.php'?>
