ae55ebde5b48a23d32a84bc0ca696bcfd0e204d9
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-19 23:39:21 +0900

    Update login.php

--- login.php
+++ login.php
@@ -2,50 +2,14 @@
 // エラーを出力する
 ini_set('display_errors', "On");
 ini_set("auto_detect_line_endings",true);
+    if(!isset($_SESSION['id'])){
+		session_start();
+	}
 //ログインしていればトップページに移動
 
 if (isset($_SESSION['name'])) {
-  header('Location: index.php');
-
-//ログインしていない場合
-}else if (isset($_POST['email']) && isset($_POST['password'])) {
-     // echo "test get post";
-    
-     // データベースに接続
-    include('source/dbconnect.php');
-      // echo "db connect";
-  try {
-    $db = new PDO($dsn, $user, $password);
-    $db ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-    //データを受け取る
-    $email=$_POST['email'] ;
-     //echo $email;
-    //hash
-		$beforehash=$_POST['email'].$_POST['password'];
-		$afterhash=hash('sha256', $beforehash);
-
-    // プリペアドステートメントを作成
-    $stmt = $db ->prepare("
-    SELECT * FROM shineva.user_kihon WHERE e_mail=:e_mail AND password=:password
-    ");
-    
-    // print_r($db->errorInfo());
-
-    // パラメータを割り当て
-    $stmt->bindParam(':e_mail',$email, PDO::PARAM_STR);
-    $stmt->bindParam(':password', $afterhash,PDO::PARAM_STR);
-    $flagforsql=true;
-
-    //クエリの実行
-    $stmt->execute();
-    // echo "db execute";
-    } catch(PDOException $e){
-        die('エラー:' . $e->getMessage());
-      }
-    }
-// ログインしていない場合はログインフォームを表示する
-
+  header('Location: index.php');       //ログインしていない場合
+}
 ?>
 
 
@@ -74,7 +38,7 @@ if (isset($_SESSION['name'])) {
 	<section class="login_in">
 	<div class="form-group">
 	
-		<form action="" method="post">
+		<form action="source/loginrealize.php" method="post">
             <label>Eメールを入力してください。</label><br>
             <input type="text" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" placeholder="xxxx@example.com" required>
 			<br><br>
@@ -103,72 +67,7 @@ if (isset($_SESSION['name'])) {
             
           <button type="submit">ログイン</button><br>
           </form>
-            
-      <?php
 
-          /**
-          * メール認証なしの時だけ：if($_COOKIE['terminal']==true)
-          * 通常：if($_COOKIE['terminal']!=true)
-          */
-        if(isset($flagforsql)){  //上記のsql分が実行したかを判定する
-          if ($row = $stmt->fetch()){
-            // ユーザが存在していたので、端末認証行う
-            if($_COOKIE['terminal']==true){
-              //メール認証
-                
-              //四文字コード生成
-              $cryp=random_int(1000,9999);
-              //５分制限
-              setcookie('cryp', $cryp, 60*5, '/');
-              //メール発送
-              include('source/sendmail.php');
-              sendmail($_SESSION['email'] ,$cryp);
-              
-            ?>
-      <label>認証コードを入力してください。</label>
-      <?php
-					if($_COOKIE["cryp"]!=$_POST['crypsecond']){
-						echo'<span color="#FF0000">認証失敗しました。もう一度メールを確認してください。\Nメールが届いてない時は再発送ボタンを押してください。</span>';
-					}else{
-						//認証できた
-						header("location: reset2.php");
-					}
-				?>
-			<br>
-      <input type="text" name="crypsecond" value="" placeholder="code"><br><br>
-			<input type="submit"><br><br>
-			<button type="submit">認証コードを確認</button>
-			
-			
-
-      <?php
-              
-            }else{  
-              $_SESSION['id'] = $row['user_id'];
-              $_SESSION['name'] = $row['user_name'];
-              $_SESSION['email'] = $_POST['email'];
-              //90日間一回ログイン
-              //setcookie('name', $row['user_name'], 60*60*24*90, '/');
-              //端末を識別するために,一年間有効
-              setcookie('terminal', 'true', 60*60*24*365, '/');
-              header('Location: index.php');
-              exit();
-            }
-          }else{
-            // 1レコードも取得できなかったとき
-            // ユーザ名・パスワードが間違っている可能性あり
-            // もう一度ログインフォームを表示
-            $acount_alert = "<script type='text/javascript'>alert('アカウント情報が間違っています.');</script>";
-            echo $acount_alert;
-            unset($_POST['email']);
-            unset($_POST['password']);
-            header('Location:login.php');
-            exit();
-        
-        }
-        }
-        
-      ?>
             <a href="reset.php">パスワードを忘れました</a><br><br>
             
             
