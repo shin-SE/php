1ec557eb7729fc3b74927bac49aa95961f25733d
Shikizakura <84487024+Shikizakura@users.noreply.github.com>
2022-01-20 14:08:24 +0900

    Update thread_head.php

--- inc/thread_head.php
+++ inc/thread_head.php
@@ -1,6 +1,6 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="../css/main.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 	<!-- 読み込み元ファイルの2行目の $title -->
    <title><?php echo $title; ?></title> 
