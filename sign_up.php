1f29d9452c084bb1aceafc1d668db9b39847d0ba
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-21 00:17:25 +0900

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -2,52 +2,74 @@
    // エラーを出力する
 	ini_set('display_errors', "On");
 	ini_set("auto_detect_line_endings",true);
-    echo 'test111';
+     
 	if (!isset($_SESSION['sync'])) {
            session_start();
            $_SESSION['sync']=true;
     }
+
   //ログインしていればトップページに移動
 	if (isset($_SESSION['name'])) {
            header('Location: index.php');
 
   //ログインしていない場合
 	}
-	echo 'test2222';
 	if (isset($_POST['email'] )) {
            //データ受け取る
-           echo 'test333';
-	       $_SESSION['email'] = $_POST['email'];
-           $email=$_SESSION['email'];
+            $_SESSION['email'] = $_POST['email'];
+            $email=$_SESSION['email'];
       	    // データベースに接続
-      		include('source/dbconnect.php');
+            include('source/dbconnect.php');
 
             try {
-                    $db = new PDO("mysql:host=" . $host. "; dbname=".$name, $user, $password );
-                    $db ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                        $db = new PDO("mysql:host=" . $host. "; dbname=".$name, $user, $password );
+                        $db ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-                    //データを受け取る
-                    $email=$_POST['email'] ;
-                    // フ゜リヘ゜アト゛ステートメントを作成
-                    $stmt = $db->prepare("SELECT * FROM user_kihon WHERE e_mail=:e_mail");
+                        //データを受け取る
+                        $email=$_POST['email'] ;
+                        // フ゜リヘ゜アト゛ステートメントを作成
+                        $stmt = $db->prepare("SELECT * FROM user_kihon WHERE e_mail=:e_mail");
 
-                    // ハ゜ラメータを割り当て
-                    $stmt->bindParam(':e_mail',$email, PDO::PARAM_STR);
-                    $flagforsql=true;
+                        // ハ゜ラメータを割り当て
+                        $stmt->bindParam(':e_mail',$email, PDO::PARAM_STR);
+                        $flagforsql=true;
 
-                    //クエリの実行
-                    $stmt->execute();
-                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                    if($result==null){
-                          $flag_nodata=true;
-                       }else if($result!=null){
-                          $flag_nodata=false;
-                       }
-	        }catch(PDOException $e){
-			        die('エラー:' . $e->getMessage());
-		    }
+                        //クエリの実行
+                        $stmt->execute();
+                        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                        if($result==null){
+                              $flag_nodata=true;
+                        }else if($result!=null){
+                              $flag_nodata=false;
+                        }
+            }catch(PDOException $e){
+                  die('エラー:' . $e->getMessage());
+            }
+      if(isset($_POST['email'])){
+            if(isset($flag_nodata)&& $flag_nodata==true){
+                        //四文字コード生成
+                        $cryp=random_int(1000,9999);
+                        $_SESSION['random']=$cryp;
+                        //５分制限
+                        //setcookie('cryp', $cryp, 60*5, '/');
+                        //メール発送
+                        include('source/sendmail.php');
+                        
+            }else if(isset($flag_nodata)&& $flag_nodata==false){
+                        echo 'このメールアドレスは既に登録していました。';
+                        unset($_POST['email']);
+            }
+            
 	}
-			
+      
+}
+if(isset($_POST['crypsecond'])){
+      $cryp_int=(int)$_POST['crypsecond'];
+      if($_SESSION['random']==$cryp_int){
+            //認証できた
+            header("Location: riyoukiyaku.php");
+      }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -70,46 +92,18 @@
 	</nav>
 	<!-- 認証コード入力設定 -->
 	<section>
-			<?php if(isset($_POST['email'])) echo $_POST['email'] ;?>
 			
     		<form action="" method="post" >
     			メールアドレスを入力してください.<br>
-    			<input type="email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email'] ;?>"><br><br>
+    			<input type="email" name="email" value=""><br><br>
     			<input type="submit" value="認証コードを送信" />
             </form>
  
-
-
        
-            <label>認証コードを入力してください。</label>
-				<?php 
-				     if(isset($_POST['email'])){
-			               if(isset($flag_nodata)&& $flag_nodata==true){
-                 					//四文字コード生成
-                 					$cryp=random_int(1000,9999);
-                 					echo $cryp;
-                 					//５分制限
-                 					setcookie('cryp', $cryp, 60*5, '/');
-                 					//メール発送
-                 					include('source/sendmail.php');
-         					}else if(isset($flag_nodata)&& $flag_nodata==false){
-         					        echo 'このメールアドレスは既に登録していました。';
-         					        unset($_POST['email']);
-				        }
-            				if(isset($_POST['crypsecond'])){
-            					if($_COOKIE["cryp"]!=$_POST['crypsecond']){
-            						  echo'<span color="#FF0000">認証失敗しました。もう一度メールを確認してください。\Nメールが届いてない時は再発送ボタンを押してください。</span>';
-            					}else if($_COOKIE["cryp"]==$_POST['crypsecond']){
-            						  //認証できた
-            						  header("location: riyoukiyaku.php");
-            					}
-            				}
-            		}
-				?>
-			<br>
+            <label>認証コードを入力してください。</label><br>
 		<form action="" method="post">
 			<input type="text" name="crypsecond" value=""><br><br>
 			<input type="submit"><br><br>
-        </form>
+            </form>
 	</section>
 <?php include 'inc/footer.php'; ?> <!-- footer.php の読み込み -->
