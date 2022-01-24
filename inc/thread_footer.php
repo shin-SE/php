72547b7201a801e29fc150d85b3357db16458ccb
memberjoho <84488773+dondcyan01@users.noreply.github.com>
2022-01-24 09:52:25 +0900

    Add files via upload

--- inc/thread_footer.php
+++ inc/thread_footer.php
@@ -1,13 +1,15 @@
-	
+
+	<div class="border"></div>
 	<!-- フッター設定 -->
-	<footer>
+	<footer id="trd_footer">
+	<div class="footer_inner">
 		<div class="foot-wrap">
 			<div class="menu">
 			<div class="menu-left">
 				<h3>問合せ</h3>
 				<ul class="foot-left">
-					<li><a href="#">管理者へメール</a></li>
-					<li><a href="#">よくある質問</a></li>
+					<li><a href="../form.php">管理者へメール</a></li>
+					<li><a href="../question.php">よくある質問</a></li>
 				</ul>
 			</div>
 			<div class="menu-right">
@@ -18,14 +20,17 @@
 				</ul>
 			</div>
 			</div>
-			<small class="cmark">©️copyright 2021
+			<small class="cmark">@copyright 2021
 				<a href="../index.php">SIN・SYSTEM ENGINEERS</a>
 			</small>
 		</div>
+		</div>
 	</footer>
-	</div>
+
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 	<script src="https://rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js"></script>
-	<script src="js/ex.js"></script>
+	<script src="../js/ex.js"></script>
+	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<script src="../js/particles.js"></script>
 </body>
 </html>
