e97baf649e7d3229421fdb824a3a02c9b1355bb6
Shikizakura <84487024+Shikizakura@users.noreply.github.com>
2022-01-25 11:08:09 +0900

    Add files via upload

--- sign_up2.php
+++ sign_up2.php
@@ -8,68 +8,75 @@ include ('source/dbconnect.php') ;
 include ('source/inputcheck.php');
 if(isset($_POST['user_name'])&&isset($_POST['gender'])){
 	//データの受け取る
-	$email = '123456789abcdefghijk@gmail.com';
-	// $email=$_SESSION['email'];
+	// $email = '0123456789abcdefghij@gmail.com';
+	$email=$_SESSION['email'];
 	$user_name=$_POST['user_name'];
 	$gender=$_POST['gender'];
 	$job=$_POST['job'];
+	
+	$is_comp = comp_pass($_POST['password'], $_POST['passwordsec']);
+	$namelegal = namelegal($user_name);
+	$passwordlegal = passwordlegal($_POST['password']);
+	
+	if ($is_comp[0] == true) {
+		if ($passwordlegal[0] == true) {
+			if($namelegal[0] == true) {
+				//日本東京タイムゾーン指定
+				date_default_timezone_set("Asia/Tokyo"); 
+				try {
+					mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+					$mysqli = mysqli_connect($host, $user, $password, $name);
 
-	//日本東京タイムゾーン指定
-	date_default_timezone_set("Asia/Tokyo"); 
-	try {
-		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-		$mysqli = mysqli_connect($host, $user, $password, $name);
+					
+					//hash
+					// $beforehash=$email.$_POST['password'];
+					$beforehash=$_SESSION['email'].$_POST['password'];
+					$afterhash=hash('sha256', $beforehash);
 
-		
-		//hash
-		$beforehash=$email.$_POST['password'];
-		// $beforehash=$_SESSION['email'].$_POST['password'];
-		$afterhash=hash('sha256', $beforehash);
-
-		// プリペアドステートメントを作成
-		$sql = "INSERT INTO user_kihon (e_mail, password, user_name, sign_in) VALUES (?, ?, ?, ?);";
-		$stmt = mysqli_prepare($mysqli, $sql);
-		$sql2 = "INSERT INTO pf (user_id, job, gender) VALUES (?, ?, ?);";
-		$stmt2 = mysqli_prepare($mysqli, $sql2);
+					// プリペアドステートメントを作成
+					$sql = "INSERT INTO user_kihon (e_mail, password, user_name, sign_in) VALUES (?, ?, ?, ?);";
+					$stmt = mysqli_prepare($mysqli, $sql);
+					$sql2 = "INSERT INTO pf (user_id, job, gender) VALUES (?, ?, ?);";
+					$stmt2 = mysqli_prepare($mysqli, $sql2);
 
 
-		// パラメータを割り当て
-		$time=date('Y-m-d H:i:s');
-		mysqli_stmt_bind_param($stmt, "ssss", $email, $afterhash, $user_name, $time);
-		
-		//クエリの実行
-		$row = mysqli_stmt_execute($stmt);
-		if ($row = true){
-			// アップデート成功、stmt2実行
-			$sql3 = 'SELECT user_id FROM user_kihon WHERE e_mail = "' . $email . '";';
-			$result = mysqli_query($mysqli, $sql3);
-			$array_row = mysqli_fetch_row($result);
-			mysqli_stmt_bind_param($stmt2, "sss", $array_row[0], $job, $gender);
-			$row2 = mysqli_stmt_execute($stmt2);
-			mysqli_close($mysqli);
-			if ($row2 = true && $is_pass=true && $is_comp=true && $is_name=true){
-				$_SESSION['id'] = $row['user_id'];
-				$_SESSION['name'] = $row['user_name'];
-				// sign_up.phpで設定済み:$_SESSION['email'] = $_POST['e_mail'];
-				// 90日間一回ログイン
-				// setcookie('email', $row['e_mail'], 60*60*24*90, '/');
-				// 端末を識別するために,一年間有効
-				setcookie('terminal', 'true', 60*60*24*365, '/');
-				header('Location: index.php');
-				exit();
+					// パラメータを割り当て
+					$time=date('Y-m-d H:i:s');
+					mysqli_stmt_bind_param($stmt, "ssss", $email, $afterhash, $user_name, $time);
+				
+					//クエリの実行
+					$row = mysqli_stmt_execute($stmt);
+					// アップデート成功、stmt2実行
+					$sql3 = 'SELECT user_id FROM user_kihon WHERE e_mail = "' . $email . '";';
+					$result = mysqli_query($mysqli, $sql3);
+					$array_row = mysqli_fetch_row($result);
+					mysqli_stmt_bind_param($stmt2, "sss", $array_row[0], $job, $gender);
+					$row2 = mysqli_stmt_execute($stmt2);
+					mysqli_close($mysqli);
+					$_SESSION['id'] = $row['user_id'];
+					$_SESSION['name'] = $row['user_name'];
+					// sign_up.phpで設定済み:$_SESSION['email'] = $_POST['e_mail'];
+					// 90日間一回ログイン
+					// setcookie('email', $row['e_mail'], 60*60*24*90, '/');
+					// 端末を識別するために,一年間有効
+					setcookie('terminal', 'true', 60*60*24*365, '/');
+					header('Location: index.php');
+					exit();
+				} catch(PDOException $e){
+					die('エラー:' . $e->getMessage());
+				}
+			} else if (ctype_space($user_name)) {
+				echo "<script type='text/javascript'>alert('空白文字だけでは登録できません。');</script>";
 			} else {
-				// 失敗、もう一度アップデートフォームを表示
-				header('Location: sign_up2.php');
-				exit();
+				echo $namelegal[1];
 			}
 		} else {
-			// 失敗、もう一度アップデートフォームを表示
-			header('Location: sign_up2.php');
-			exit();
-        }
-	} catch(PDOException $e){
-		die('エラー:' . $e->getMessage());
+			echo $passwordlegal[1];
+		}
+	} else {
+		echo $is_comp[1];
 	}
+	
 }
 ?>
 <!DOCTYPE html>
@@ -92,9 +99,13 @@ include 'inc/head.php'; // head.php の読み込み
 		<li class="crumb">Sign Up2</li>
 	</ol>
 </nav>
-
+<?php
+var_dump($is_comp[0]);
+var_dump($passwordlegal[0]);
+var_dump($namelegal[0]);
+var_dump(ctype_space($user_name));
+?>
 <section>
-
 	<form action="" method="post">
 		<!--ニックネーム入力-->
 		<label>ニックネームを入力してください。20文字以内。</label><br>
@@ -108,7 +119,7 @@ include 'inc/head.php'; // head.php の読み込み
 		</select>
 		<br><br>
 		<!-- パスワード一回目の入力 -->
-		<label>パスワードを入力してください。数字、英文字含めて8~20文字</label><br><br>
+		<label>パスワードを入力してください。数字、小文字、大文字含めて8~16文字</label><br><br>
 		<input type="password" name="password" value="" placeholder="password"><br><br>
 		<!-- パスワード二回目の入力 -->
 		<label>確認のためもう一度入力してください。</label><br>
