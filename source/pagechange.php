09cd544b90bf270be0a2dbb1c30eb10ed10763f4
rau42 <84487730+rau42@users.noreply.github.com>
2022-02-02 09:17:49 +0900

    Update pagechange.php

--- source/pagechange.php
+++ source/pagechange.php
@@ -1,4 +1,4 @@
-
+<div class="pag_div">
 <div class="pagination">
       <a href="?pageno=1" class="previous" style="display:inline">&nbsp;&laquo;&nbsp;最初&nbsp;</a>
       <div class="<?php if($pageno <= 1){ echo 'disabled'; } ?> " style="display:inline">
@@ -9,4 +9,5 @@
             <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>" class="next round">&nbsp;&nbsp;&#8250;&nbsp;&nbsp;</a>
       </div>
       <a href="?pageno=<?php echo $total_pages; ?>" class="next" style="display:inline">&nbsp;最後&nbsp;&raquo;&nbsp;</a>
-</udiv>
+</div>
+</div>
